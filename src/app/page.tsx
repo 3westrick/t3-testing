8f0a176be0c3ec167ccf5510a963c6728ae9bf0d
Arad Afraz <threewestrick@gmail.com>
2025-03-07 13:00:38 +0330

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   'https://lommqduyua.ufs.sh/f/qKdRrWtezHInKh9eSRbn3CI2PMhftO7sL8y5BmgFlrHNkJ6u',
@@ -13,16 +14,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany()
+  console.log(posts)
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
+      {/* <div className="flex flex-wrap gap-4">
         {[...mockImages, ...mockImages, ...mockImages].map(image => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image"/>
           </div>
         ))}
-      </div>
+      </div> */}
     </main>
   );
 }
