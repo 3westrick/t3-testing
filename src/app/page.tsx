836a72fb014809aac70ded9e7fe0ab2f72879bc1
Arad Afraz <threewestrick@gmail.com>
2025-03-07 13:08:59 +0330

    showing posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,17 @@ export default async function HomePage() {
   console.log(posts)
   return (
     <main className="">
-      {/* <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map(image => (
-          <div key={image.id} className="w-48">
+
+      <div className="flex flex-wrap gap-4">
+        {posts.map(post => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={index} className="w-48">
             <img src={image.url} alt="image"/>
           </div>
         ))}
-      </div> */}
+      </div>
     </main>
   );
 }
